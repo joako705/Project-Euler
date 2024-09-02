f3695e6076a69bec2cf886cbe5bbd8f33d576725
joako705 <jperez070705@gmail.com>
2024-09-02 08:20:14 -0600

    145 solved

---
+++ problems/145.cpp
@@ -0,0 +1,37 @@
+//reversible numbers: numbers for which (n + reverse(n)) has only odd numbers
+//find how many reversible numbers exist below 1 billion
+
+#include <iostream>
+#include <cmath>
+
+unsigned int reverse_num(unsigned int x) {
+    unsigned int result = 0;
+
+    while (x > 0) {
+        result *= 10;
+        result += x % 10;
+        x /= 10;
+    }
+
+    return result;
+}
+
+bool all_odd(unsigned int x) {
+    while (x > 0) {
+        if (x % 2 == 0) return false;
+        x /= 10;
+    }
+
+    return true;
+}
+
+int main() {
+    unsigned int count = 0;
+
+    for (unsigned int i = 11; i < pow(10, 8); i += 2) {
+        if (all_odd(i + reverse_num(i))) count += 2;
+    }
+
+    std::cout << count << std::endl;
+    return 0;
+}
