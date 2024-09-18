41f55d146f1ba4c018b4c3b257262d433cbcb11a
jp-zuniga <jperez070705@gmail.com>
2024-09-17 22:33:29 -0600

    renamed variable

--- solutions/016.py
+++ solutions/016.py
@@ -6,8 +6,8 @@ digit_sum = 0
 big_num = 2**1000
 
 # convert the number to a string, iterate through every digit, and add them all up
-string = str(big_num)
-for digit in string:
+num_string = str(big_num)
+for digit in num_string:
     digit_sum += int(digit)
 
 print(f"Sum of the digits of 2^1000: {digit_sum}")
